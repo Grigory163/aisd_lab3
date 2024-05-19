c7970285380609aee8f509ff061d075a5f6fdc6c
Grigory <kotenkovgrigorij@gmail.com>
2024-05-19 19:04:31 +0300

    shrppth_bel_ford

--- aisd_lab3/aisd_lab3.cpp
+++ aisd_lab3/aisd_lab3.cpp
@@ -7,9 +7,31 @@ using namespace std;
 
 int main()
 {
-	Graph<int, int> a;
-	a.add_vertex(1);
-	std::cout << a.has_vertex(1);
-	auto b = a.vertices();
-	std::cout << b.capacity();
+    Graph<char, int> graph;
+
+    // Добавление вершин
+    graph.add_vertex('A');
+    graph.add_vertex('B');
+    graph.add_vertex('C');
+    graph.add_vertex('D');
+    graph.add_vertex('E');
+
+    // Добавление ребер
+    graph.add_edge( 'A', 'B', 5 );
+    graph.add_edge( 'A', 'C', 3 );
+    graph.add_edge('B', 'D', 2 );
+    graph.add_edge('C', 'B', 1);
+    graph.add_edge( 'C', 'D', 7 );
+    graph.add_edge( 'D', 'E', 4);
+
+    // Нахождение кратчайшего пути от вершины 'A' до вершины 'E'
+    auto shortest_path = graph.shortest_path('A', 'E');
+
+    // Вывод кратчайшего пути
+    std::cout << "Shortest Path from A to E:" << std::endl;
+    for (const auto& edge : shortest_path) {
+        std::cout << edge.source << " -> " << edge.destination << " : " << edge.weight << std::endl;
+    }
+
+    return 0;
 }
--- aisd_lab3/aisd_lab3.h
+++ aisd_lab3/aisd_lab3.h
@@ -19,6 +19,10 @@ public:
 		Vertex source;
 		Vertex destination;
 		Distance weight;
+		bool operator==(const Edge& other) const
+		{
+			return (source == other.source) && (destination == other.destination) && (weight == other.weight);
+		}
 	};
 private:
 	std::vector<Vertex> _vertices;
@@ -65,4 +69,139 @@ public:
 
 	//проверка-добавление-удаление ребер
 
+	bool has_edge(const Vertex& _from, const Vertex& _to) const
+	{
+		for (const auto& edge : _edges)
+		{
+			if (edge.source == _from && edge.destination == _to)
+				return true;
+		}
+		return false;
+	}
+
+	bool has_edge(const Edge& e) const // с учетом расстояния
+	{
+		return std::find(_edges.begin(), _edges.end(), e) != _edges.end();
+	}
+
+	void add_edge(const Vertex& from, const Vertex& to, const Distance& d)
+	{
+		if (!has_edge({ from, to, d }))
+			_edges.push_back({ from, to, d });
+	}
+
+	bool remove_edge(const Vertex& from, const Vertex& to)
+	{
+		if (!has_edge(from, to))
+			return false;
+
+		for (auto it = _edges.begin(); it != _edges.end();)
+		{
+			if (it->source == from && it->destination == to)
+			{
+				it = _edges.erase(it);
+			}
+			else
+				it++;
+		}
+		return true;
+	}
+
+	bool remove_edge(const Edge& e)// с учетом расстояния
+	{
+		auto it = std::find(_edges.begin(), _edges.end(), e);
+		if (it == _edges.end())
+			return false;
+
+		_edges.erase(it);
+
+		return true;
+	}
+
+	//получение всех ребер, выходящих из вершины
+
+	std::vector<Edge> edges(const Vertex& vertex)
+	{
+		std::vector<Edge> res_edges;
+		for (auto& edge : _edges)
+		{
+			if (edge.source == vertex)
+				res_edges.push_back(edge);
+		}
+		return res_edges;
+	}
+
+	size_t order() const
+	{
+		return _vertices.size();
+	}
+
+
+	size_t degree(const Vertex& v) const
+	{
+		size_t count = 0;
+		for (auto& edge : _edges)
+		{
+			if (edge.source == v || edge.destination == v)
+				count++;
+		}
+		return count;
+	}
+
+	//поиск кратчайшего пути
+
+	std::vector<Edge> shortest_path(const Vertex& from, const Vertex& to) const
+	{
+		std::vector<Distance> distance(_vertices.size(), std::numeric_limits<Distance>::max());
+		std::vector<Vertex> predecessor(_vertices.size(), Vertex());
+
+		distance[std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), from))] = 0;
+
+		for (size_t i = 0; i < _vertices.size() - 1; ++i)
+		{
+			for (const auto& edge : _edges)
+			{
+				size_t u = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), edge.source));
+				size_t v = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), edge.destination));
+				Distance weight = edge.weight;
+
+				if (distance[u] + weight < distance[v])
+				{
+					distance[v] = distance[u] + weight;
+					predecessor[v] = edge.source;
+				}
+			}
+		}
+
+		for (const auto& edge : _edges)
+		{
+			size_t u = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), edge.source));
+			size_t v = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), edge.destination));
+			Distance weight = edge.weight;
+
+			if (distance[u] + weight < distance[v])
+			{
+				// Negative cycle detected
+				return {}; // Return an empty path
+			}
+		}
+
+		std::vector<Edge> path;
+		size_t current = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), to));
+		while (predecessor[current] != Vertex())
+		{
+			Vertex pred = predecessor[current];
+			auto it = std::find_if(_edges.begin(), _edges.end(), [pred, current, this](const Edge& edge) {
+				return edge.source == pred && edge.destination == this->_vertices[current];
+				});
+			if (it != _edges.end())
+			{
+				path.push_back(*it);
+				current = std::distance(_vertices.begin(), std::find(_vertices.begin(), _vertices.end(), pred));
+			}
+		}
+
+		std::reverse(path.begin(), path.end());
+		return path;
+	}
 }; 
